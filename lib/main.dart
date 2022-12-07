3a9ae944feadacd7ea9b6d909f42fa60d6e5952a
taras3189 <taras3189@gmail.com>
2022-12-07 12:04:53 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 final ThemeData theme = ThemeData();
 
@@ -13,10 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Daily Readings',
       theme: theme.copyWith(
-          colorScheme: theme.colorScheme.copyWith(
-            primary: Color(0xff477bab),
-          ),
-          backgroundColor: Colors.white),
+        colorScheme: theme.colorScheme.copyWith(
+          primary: const Color(0xff477bab),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
     );
